bd501b3c349014bee96428aa6ecf907b984bdc96
Mathpal <deepak.mathpal@accenture.com>
2023-04-03 13:12:33 +0530

    compare code

---
+++ src/test/java/Thread/runCompare.java
@@ -0,0 +1,73 @@
+package Thread;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static pageHelper.getMutualFundData.getActualDataFromFundPageAsAlIst;
+
+
+public class runCompare {
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] urls = {"https://www.mfs.com/en-us/individual-investor/product-strategies/mutual-funds/MUSEX-mfs-blended-research-core-equity-fund.html",
+                "https://www.mfs.com/en-us/individual-investor/product-strategies/mutual-funds/BRWJX-mfs-blended-research-growth-equity-fund.html"};
+
+        String[] services = {"[8.0,11.58,8.26,7.8,-16.06,2.52], [null,12.56,9.42,7.66,-18.11,null], [8.0,11.58,8.26,7.8,-16.06,-16.06], [null,12.56,9.42,7.66,-18.11,-18.11]",
+                "[10.38,null,8.34,5.81,-29.76,3.25], [null,null,10.96,7.79,-29.14,null], [10.38,null,8.34,5.81,-29.76,-29.76], [null,null,10.96,7.79,-29.14,-29.14]"};
+
+        String[] excel = {"[8.0,11.58,8.26,7.8,-16.06,2.52], [null,12.56,9.42,7.66,-18.11,null], [8.0,11.58,8.26,7.8,-16.06,-16.06], [null,12.56,9.42,7.66,-18.11,-18.11]",
+                "[10.38,null,8.34,5.81,-29.76,3.25], [null,null,10.96,7.79,-29.14,null], [10.38,null,8.34,5.81,-29.76,-29.76], [null,null,10.96,7.79,-29.14,-29.14]"};
+
+        List<String> resultsFromUI = new ArrayList<>();
+        List<String> resultsFromServices = new ArrayList<>();
+        List<String> resultsFromExcel = new ArrayList<>();
+
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String url: urls) {
+                    System.out.println("Running from :" + url);
+                    try {
+                        resultsFromUI.addAll(getActualDataFromFundPageAsAlIst(url));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String url: urls) {
+                    System.out.println("Running from :" + url);
+                        resultsFromServices.addAll(Arrays.asList(services));
+                    }
+            }
+        });
+
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String url: urls) {
+                    System.out.println("Running from :" + url);
+                    resultsFromExcel.addAll(Arrays.asList(excel));
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println(resultsFromUI);
+        System.out.println(resultsFromServices);
+        System.out.println(resultsFromExcel);
+    }
+}
